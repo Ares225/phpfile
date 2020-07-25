7cfbb606b8119a5a8b06dd328ea8e3f1f9729ff3
Ayush Kumar <68781382+Ares225@users.noreply.github.com>
2020-07-25 22:01:36 +0530

    Add files via upload

---
+++ inputtype.php
@@ -0,0 +1,12 @@
+<?php
+echo "<input type='text' name='name' value='Ares'>";
+print "<input type='text' name='name' value='Ayush'>";
+echo"<input type=\"button\" value=\"change\">";
+$day='saturday';
+$date=25;
+$year=2020;
+echo '<strong>today is '.$day.' '.$date.' '.$year.'</strong>';
+
+ echo "<strong>today is $day $date $year '</strong>'";
+
+?>
